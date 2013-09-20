06381dafe42f35808cb35b13b1dae632a4c111e4
flukus <flukus@gmail.com>
2013-09-20 13:42:31 +1000

    added test project

---
+++ src/test/TrueUnit.Test/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using TrueUnit.Framework;
+
+namespace TrueUnit.Test
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var extractor = new TestExtractor();
+			var tests = extractor.ExtractTests(new Assembly[] { typeof(Program).Assembly });
+			var runner = new TestRunner();
+			runner.Execute(tests);
+		}
+	}
+}
